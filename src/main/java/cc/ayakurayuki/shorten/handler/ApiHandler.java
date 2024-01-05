03f330a562efe4f953ef342c05f5ce854069a97c
Ayakura Yuki <YukiAyakura@outlook.com>
2024-01-05 14:10:41 +0800

    @ repo

--- src/main/java/cc/ayakurayuki/shorten/handler/ApiHandler.java
+++ src/main/java/cc/ayakurayuki/shorten/handler/ApiHandler.java
@@ -5,8 +5,11 @@ import cc.ayakurayuki.shorten.core.Consts;
 import cc.ayakurayuki.shorten.model.SaveURLReq;
 import cc.ayakurayuki.shorten.model.SaveURLRsp;
 import cc.ayakurayuki.shorten.repository.LinkRepository;
+import cc.ayakurayuki.shorten.util.RandomUtils;
 import cc.ayakurayuki.shorten.util.UrlUtils;
 import io.vertx.core.Future;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -26,19 +29,30 @@ public class ApiHandler {
   @Path("/save-url")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  public Future<SaveURLRsp> saveURL(SaveURLReq req) {
+  public Future<SaveURLRsp> saveURL(SaveURLReq req) throws MalformedURLException {
     if (req.getUrl() == null || req.getUrl().isBlank()) {
       throw new RuntimeException("Bad Request");
     }
 
     boolean fromAdmin = UrlUtils.checkHash(req.getUrl(), req.getHash());
 
+    URL url = new URL(req.getUrl());
+    String host = url.getHost();
+    boolean isWhite = UrlUtils.checkWhiteList(host);
+
+    String key = req.getKey();
+    if (!fromAdmin || key == null || key.isBlank()) {
+      key = RandomUtils.generateKey(Consts.DEFAULT_KEY_LENGTH);
+    }
+
     Duration ttl = null;
     if (req.getTtlSecond() != null && req.getTtlSecond() > 0) {
       ttl = Duration.ofSeconds(req.getTtlSecond());
+    } else if (!fromAdmin && !isWhite) {
+      ttl = Duration.ofDays(30);
     }
 
-    return repo.save(req.getKey(), req.getUrl(), ttl, Consts.DEFAULT_KEY_LENGTH, fromAdmin);
+    return repo.save(key, req.getUrl(), ttl, fromAdmin, isWhite);
   }
 
 }
--- src/main/java/cc/ayakurayuki/shorten/repository/LinkRepository.java
+++ src/main/java/cc/ayakurayuki/shorten/repository/LinkRepository.java
@@ -2,16 +2,11 @@ package cc.ayakurayuki.shorten.repository;
 
 
 import cc.ayakurayuki.shorten.core.Application;
-import cc.ayakurayuki.shorten.core.Consts;
 import cc.ayakurayuki.shorten.model.Link;
 import cc.ayakurayuki.shorten.model.SaveURLRsp;
-import cc.ayakurayuki.shorten.util.RandomUtils;
-import cc.ayakurayuki.shorten.util.UrlUtils;
 import io.vertx.core.Future;
 import io.vertx.core.json.Json;
 import io.vertx.redis.client.RedisAPI;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 
@@ -40,59 +35,37 @@ public class LinkRepository {
       });
   }
 
-  public Future<SaveURLRsp> save(String key, String link, Duration ttl, int keyLength, boolean fromAdmin) {
-    String host;
-    try {
-      URL url = new URL(link);
-      host = url.getHost();
-    } catch (MalformedURLException e) {
-      return Future.failedFuture(e);
-    }
-
-    if (keyLength == 0) {
-      keyLength = Consts.DEFAULT_KEY_LENGTH;
-    }
-
-    if (!fromAdmin) {
-      key = RandomUtils.generateKey(keyLength);
-    }
-
-    String finalKey = key;
-    int finalKeyLength = keyLength;
-    boolean isWhite = UrlUtils.checkWhiteList(host);
-    if (!fromAdmin && !isWhite) {
-      ttl = Duration.ofDays(30);
-    }
-    Duration finalTTL = ttl;
-
+  public Future<SaveURLRsp> save(String key, String link, Duration ttl, boolean fromAdmin, boolean isWhite) {
     return load(key)
       .compose(existLink -> {
         if (existLink != null) {
-          return save(finalKey, link, finalTTL, finalKeyLength + 1, fromAdmin);
+          return save(key, link, ttl, fromAdmin, isWhite);
         }
+
+        long nowSeconds = System.currentTimeMillis() / 1000;
 
         Link metaLink = new Link();
         metaLink.setUrl(link);
-        metaLink.setCreateAt(System.currentTimeMillis() / 1000);
-        if (finalTTL != null) {
-          metaLink.setExpireAt(System.currentTimeMillis() / 1000 + finalTTL.toSeconds());
-        } else {
-          metaLink.setExpireAt(-1L);
-        }
+        metaLink.setCreateAt(nowSeconds);
+        metaLink.setExpireAt(ttl != null ? nowSeconds + ttl.toSeconds() : -1L);
         metaLink.setFromAdmin(fromAdmin);
 
         String json = Json.encodePrettily(metaLink);
 
         SaveURLRsp rsp = new SaveURLRsp();
-        rsp.setKey(finalKey);
+        rsp.setKey(key);
 
         if (!fromAdmin && !isWhite) {
-          long seconds = finalTTL != null ? finalTTL.toSeconds() : Duration.ofDays(30).toSeconds();
-          redis.setex(finalKey, String.valueOf(seconds), json);
+
+          long seconds = ttl != null ? ttl.toSeconds() : Duration.ofDays(30).toSeconds();
+          redis.setex(key, String.valueOf(seconds), json);
           rsp.setExpireAt(System.currentTimeMillis() / 1000 + seconds);
+
         } else {
-          redis.set(List.of(finalKey, json));
+
+          redis.set(List.of(key, json));
           rsp.setExpireAt(-1L);
+
         }
 
         return Future.succeededFuture(rsp);
